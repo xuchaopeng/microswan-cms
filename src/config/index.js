09875cbdbbbc01418a3cc55e4c94cfbca1de25b0
xcp <m18770074875_1@163.com>
2019-11-22 20:15:51 +0800

    修改线上配置

--- src/config/index.js
+++ src/config/index.js
@@ -17,8 +17,8 @@ export default {
    * @description api请求基础路径
    */
   baseUrl: {
-    dev: 'http://localhost:3000',
-    pro: 'https://produce.com'
+    dev: 'http://xcpeng.cn',
+    pro: 'http://xcpeng.cn'
   },
   /**
    * @description 默认打开的首页的路由name值，默认为home
